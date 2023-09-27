150647784417ace38f872c06bd58da779d929130
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2023-09-27 15:08:19 +0800

    Refactor rollup plugin

---
+++ packages/rollup/src/index.ts
@@ -0,0 +1,24 @@
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import { Plugin, RollupOptions } from 'rollup'
+import external from 'rollup-plugin-peer-deps-external'
+
+export { commonjs, external, json, resolve, terser, typescript }
+
+export function rollup(input: string, output: string = 'build/index.js', plugins?: Plugin[]): RollupOptions {
+  return {
+    input,
+    output: [{ file: output, format: 'esm', sourcemap: true, inlineDynamicImports: true }],
+    plugins: plugins ?? [
+      external(),
+      resolve({ preferBuiltins: false }),
+      commonjs(),
+      json(),
+      typescript(),
+      terser()
+    ]
+  }
+}
